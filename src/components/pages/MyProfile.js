29e97489bbd2d428b854278550594f79cf2e3271
Rebelzob <karimdavid07@gmail.com>
2023-08-28 22:05:01 +0100

    Added my profile component

---
+++ src/components/pages/MyProfile.js
@@ -0,0 +1,18 @@
+const MyProfile = () => (
+  <section>
+    <article>
+      <h2>My Missions</h2>
+      <ul>
+        <li>missions</li>
+      </ul>
+    </article>
+    <article>
+      <h2>My Rockets</h2>
+      <ul>
+        <li>rockets</li>
+      </ul>
+    </article>
+  </section>
+);
+
+export default MyProfile;
