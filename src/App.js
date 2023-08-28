a35b87fba083270969e75602312322bd1d6c3859
Rebelzob <karimdavid07@gmail.com>
2023-08-28 22:21:39 +0100

    Added component to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Missions from './components/pages/Missions';
 import MyProfile from './components/pages/MyProfile';
+import Rockets from './components/pages/Rockets';
 
 const App = () => (
   <div className="main_container">
@@ -10,6 +11,7 @@ const App = () => (
     <section>
       <Routes>
         <Route path="/missions" element={<Missions />} />
+        <Route path="/rockets" element={<Rockets />} />
         <Route path="/myprofile" element={<MyProfile />} />
       </Routes>
     </section>
