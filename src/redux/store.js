6a8992d07d1cfe40a3882690183a3e1549a4fd96
Rebelzob <karimdavid07@gmail.com>
2023-08-29 17:43:43 +0100

    Changed names of slice because of a typo

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rocketReducer from './rockets/rocketSlice';
+import rocketReducer from './rockets/rocketsSlice';
 import missionsReducer from './missions/missionsSlice';
 
 const store = configureStore({
