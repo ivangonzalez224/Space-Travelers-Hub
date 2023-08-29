9c339870b3a98e5968892c73668b25a4a78aaae6
Rebelzob <karimdavid07@gmail.com>
2023-08-29 17:41:27 +0100

    Created reservation btn component

---
+++ src/components/RocketReservationBtn.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { reserveRocket, cancelReservation } from '../redux/rockets/rocketsSlice';
+
+const RocketReservationBtn = ({ rocketId }) => {
+  const dispatch = useDispatch();
+  const reserved = useSelector((state) => {
+    const rocket = state.rockets.rockets.find((rocket) => rocket.id === rocketId);
+    return rocket.reserved;
+  });
+
+  const reserveRocketAction = () => {
+    dispatch(reserveRocket(rocketId));
+  };
+  const cancelReservationAction = () => {
+    dispatch(cancelReservation(rocketId));
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={reserved ? cancelReservationAction : reserveRocketAction}
+      className={reserved ? 'btn btn-danger' : 'btn btn-success'}
+    >
+      {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+    </button>
+  );
+};
+
+RocketReservationBtn.propTypes = {
+  rocketId: PropTypes.number.isRequired,
+};
+
+export default RocketReservationBtn;
