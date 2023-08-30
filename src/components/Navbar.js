4d084b262fa2b612b3f94156991e5a837a62f392
Rebelzob <karimdavid07@gmail.com>
2023-08-30 19:45:30 +0100

    Added styles to the navbar with boostrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,60 @@
 import { NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
 import mainIcon from './images/planet.png';
+import '../assets/styles/Navbar.css';
 
-const links = [
-  { path: '/', text: 'Rockets' },
-  { path: 'missions', text: 'Missions' },
-  { path: 'myProfile', text: 'My Profile' },
-];
-
-const Navbar = () => (
-  <header>
-    <nav>
-      <img src={mainIcon} alt="user icon" />
-      <h1>Space Travelers&apos; Hub</h1>
-      <ul>
-        {links.map((link) => (
-          <li key={link.text}>
-            <NavLink to={link.path}>{link.text}</NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </header>
+const NavBar = () => (
+  <>
+    <Navbar bg="white" expand="lg" className="mt-4">
+      <Container>
+        <div className="d-flex gap-3">
+          <Image
+            src={mainIcon}
+            alt="user icon"
+            style={{ width: '75px', height: '75px' }}
+          />
+          <Navbar.Brand style={{ fontSize: '38px' }}>Space Travelers&apos; Hub</Navbar.Brand>
+        </div>
+        <Nav
+          className="justify-content-end align-items-center gap-3"
+          style={{ fontSize: '22px' }}
+        >
+          <Nav.Item>
+            <Nav.Link
+              as={NavLink}
+              to="/"
+              activeClassName="active-link "
+            >
+              Rockets
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              as={NavLink}
+              to="/missions"
+              activeClassName="active-link"
+            >
+              Missions
+            </Nav.Link>
+          </Nav.Item>
+          <div className="nav-separator">|</div>
+          <Nav.Item>
+            <Nav.Link
+              as={NavLink}
+              to="/myProfile"
+              activeClassName="active-link"
+            >
+              My Profile
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </Navbar>
+    <hr className="my-4 mx-5" />
+  </>
 );
 
-export default Navbar;
+export default NavBar;
