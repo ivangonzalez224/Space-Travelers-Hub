11d3b83e9b1a6ae50bed6604e5111c9d61750074
Rebelzob <karimdavid07@gmail.com>
2023-08-30 23:21:58 +0100

    Added logic and bootstrap styling

--- src/components/RocketsProfile.js
+++ src/components/RocketsProfile.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 import { useSelector } from 'react-redux';
 
 const RocketsProfile = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
 
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
   return (
-    <>
-      {rockets.filter((rocket) => rocket.reserved).map((rocket) => (
-        <li key={rocket.rocket_id}>{rocket.rocket_name}</li>
-      ))}
-    </>
+    <ListGroup>
+      {reservedRockets.length > 0 ? (
+        reservedRockets.map((rocket) => (
+          <ListGroup.Item
+            key={rocket.rocket_id}
+          >
+            {rocket.rocket_name}
+          </ListGroup.Item>
+        ))
+      ) : (
+        <h6>No Rockets reserved yet</h6>
+      )}
+    </ListGroup>
   );
 };
 
