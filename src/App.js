e9fb787e3c791404706a5ee820fe6a42f39f6e26
Rebelzob <karimdavid07@gmail.com>
2023-08-30 19:44:49 +0100

    Changed component name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
+import NavBar from './components/Navbar';
 import Missions from './components/pages/Missions';
 import MyProfile from './components/pages/MyProfile';
 import Rockets from './components/pages/Rockets';
 
 const App = () => (
   <BrowserRouter>
-    <Navbar />
+    <NavBar />
     <main>
       <Routes>
         <Route path="/" element={<Rockets />} />
