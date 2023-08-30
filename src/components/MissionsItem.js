3b96706401d7b553f15a60e489deed0c7666cc40
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-08-30 13:26:47 -0500

    Add styles to the missions table using react bootstrap

--- src/components/MissionsItem.js
+++ src/components/MissionsItem.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 import { joinMission, leaveMission } from '../redux/missions/missionsSlice';
 
 const MissionsItem = (
@@ -19,31 +21,34 @@ const MissionsItem = (
     dispatch(leaveMission(missionId));
   };
   return (
-    <li>
-      <span className="missionName">{missionName}</span>
-      <span className="missionDescrip">{description}</span>
-      <span className="status_container">
-        <p
-          className={statusCheck ? 'missionStatus status_join' : 'missionStatus status_leave'}
+    <tr>
+      <td className="missionName">{missionName}</td>
+      <td className="missionDescrip">{description}</td>
+      <td className="align-middle">
+        <Badge
+          bg={statusCheck ? 'info' : 'secondary'}
         >
           {statusCheck ? 'Active Member' : 'NOT A MEMBER'}
-        </p>
-      </span>
-      <div className="missionbuttons">
-        <button
+        </Badge>
+      </td>
+      <td className="align-middle">
+        <Button
+          variant={statusCheck ? 'outline-danger' : 'outline-secondary'}
           type="button"
           onClick={statusCheck ? leaveSingleMission : joinSingleMission}
           className={statusCheck ? 'mission_btn btn_join' : 'mission_btn btn_leave'}
         >
           {statusCheck ? 'Leave Mission' : 'Join Mission'}
-        </button>
-      </div>
-    </li>
+        </Button>
+      </td>
+    </tr>
   );
 };
+
 MissionsItem.propTypes = {
   missionId: PropTypes.string.isRequired,
   missionName: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
+
 export default MissionsItem;
